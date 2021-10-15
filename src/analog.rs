8384884997e07c81b50d99bb1bf1185dd1722e63
PotatoTech <42846367+PotatoTech@users.noreply.github.com>
2021-10-15 14:03:31 -0700

    Add digital interface for analog inputs

--- src/analog.rs
+++ src/analog.rs
@@ -8,7 +8,7 @@ pub const ANALOG_MIN: f32 = -1.0;
 /// The maximum value of an analog input.
 pub const ANALOG_MAX: f32 = 1.0;
 
-/// Wrapper around `f32`.
+/// Wrapper around `f32` for analog inputs.
 #[derive(Default, Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub(crate) struct AnalogInputValue(f32);
 
@@ -35,13 +35,40 @@ impl From<AnalogInputValue> for f32 {
     }
 }
 
+/// Wrapper around `f32` for deadzones.
+#[derive(Default, Debug, Clone, Copy, PartialEq, PartialOrd)]
+struct Deadzone(f32);
+
+impl From<AnalogInputValue> for Deadzone {
+    fn from(value: AnalogInputValue) -> Self {
+        Self(value.0.abs())
+    }
+}
+
+impl From<Deadzone> for f32 {
+    fn from(value: Deadzone) -> Self {
+        value.0
+    }
+}
+
+enum DeadzoneTransition {
+    Activated,
+    Deactivated,
+    None,
+}
+
 /// Container for analog inputs.
 #[derive(Debug)]
 pub struct AnalogInput<T> {
     inputs: HashMap<T, AnalogInputValue>,
+
     just_activated: HashSet<T>,
     just_deactivated: HashSet<T>,
-    activation_threshold: AnalogInputValue,
+    deadzone: Deadzone,
+
+    just_activated_digital: HashSet<T>,
+    just_deactivated_digital: HashSet<T>,
+    deadzone_digital: Deadzone,
 }
 
 impl<T> AnalogInput<T>
@@ -50,16 +77,15 @@ where
 {
     /// Gets the value of an analog input.
     ///
-    /// Defaults to returning `0.0` if the input has not been read yet.
+    /// Returns `0.0` if the input is within the analog deadzone, or if it has not been read yet.
     pub fn value(&self, input: T) -> f32 {
-        if let Some(&value) = self.inputs.get(&input) {
-            f32::from(value)
-        } else {
-            0.0
+        match self.inputs.get(&input) {
+            Some(&value) if Deadzone::from(value) > self.deadzone => f32::from(value),
+            _ => 0.0,
         }
     }
 
-    /// Checks if an analog input just left the deadzone.
+    /// Checks if an analog input just left the analog deadzone.
     pub fn just_activated(&self, input: T) -> Option<f32> {
         if self.just_activated.contains(&input) {
             Some(self.value(input))
@@ -68,9 +94,40 @@ where
         }
     }
 
-    /// Checks if an analog input just entered the deadzone.
+    /// Checks if an analog input just entered the analog deadzone.
     pub fn just_deactivated(&self, input: T) -> bool {
         self.just_deactivated.contains(&input)
+    }
+
+    /// Converts an analog input to a digital value.
+    ///
+    /// Returns either `ANALOG_MIN` or `ANALOG_MAX` when the input is outside the digital deadzone,
+    /// and `0.0` otherwise.
+    pub fn value_digital(&self, input: T) -> f32 {
+        match self.inputs.get(&input) {
+            Some(&value) if Deadzone::from(value) > self.deadzone_digital => {
+                if f32::from(value) < 0.0 {
+                    ANALOG_MIN
+                } else {
+                    ANALOG_MAX
+                }
+            }
+            _ => 0.0,
+        }
+    }
+
+    /// Checks if an analog input just left the digital deadzone.
+    pub fn just_activated_digital(&self, input: T) -> Option<f32> {
+        if self.just_activated_digital.contains(&input) {
+            Some(self.value_digital(input))
+        } else {
+            None
+        }
+    }
+
+    /// Checks if an analog input just entered the digital deadzone.
+    pub fn just_deactivated_digital(&self, input: T) -> bool {
+        self.just_deactivated_digital.contains(&input)
     }
 }
 
@@ -80,34 +137,71 @@ where
 {
     pub(crate) fn set(&mut self, input: T, value: AnalogInputValue) {
         let old_value = self.inputs.insert(input, value);
-        let value = f32::from(value);
-        let activation_threshold = f32::from(self.activation_threshold);
 
-        if let Some(old_value) = old_value {
-            let old_value = f32::from(old_value);
-            if value.abs() < activation_threshold {
-                self.just_activated.remove(&input);
-                if old_value.abs() >= activation_threshold {
-                    self.just_deactivated.insert(input);
-                }
-            } else {
+        match Self::crossed_deadzone(value, old_value, self.deadzone) {
+            DeadzoneTransition::Activated => {
+                self.just_activated.insert(input);
                 self.just_deactivated.remove(&input);
-                // It is possible for an analog input to completely pass through the deadzone
-                // between updates. In that case, both the old and new values would exceed the
-                // activation threshold, but they would have opposite signs.
-                if old_value.abs() < activation_threshold || value.signum() != old_value.signum() {
-                    self.just_activated.insert(input);
-                }
             }
-        } else if value.abs() >= activation_threshold {
-            self.just_activated.insert(input);
-            self.just_deactivated.remove(&input);
+            DeadzoneTransition::Deactivated => {
+                self.just_activated.remove(&input);
+                self.just_deactivated.insert(input);
+            }
+            DeadzoneTransition::None => {
+                self.just_activated.remove(&input);
+                self.just_deactivated.remove(&input);
+            }
+        }
+
+        match Self::crossed_deadzone(value, old_value, self.deadzone_digital) {
+            DeadzoneTransition::Activated => {
+                self.just_activated_digital.insert(input);
+                self.just_deactivated_digital.remove(&input);
+            }
+            DeadzoneTransition::Deactivated => {
+                self.just_activated_digital.remove(&input);
+                self.just_deactivated_digital.insert(input);
+            }
+            DeadzoneTransition::None => {
+                self.just_activated_digital.remove(&input);
+                self.just_deactivated_digital.remove(&input);
+            }
         }
     }
 
     pub(crate) fn update(&mut self) {
         self.just_activated.clear();
         self.just_deactivated.clear();
+        self.just_activated_digital.clear();
+        self.just_deactivated_digital.clear();
+    }
+
+    fn crossed_deadzone(
+        new_value: AnalogInputValue,
+        old_value: Option<AnalogInputValue>,
+        deadzone: Deadzone,
+    ) -> DeadzoneTransition {
+        let new_value = f32::from(new_value);
+        let deadzone = f32::from(deadzone);
+
+        if let Some(old_value) = old_value {
+            let old_value = f32::from(old_value);
+
+            if new_value.abs() <= deadzone && old_value.abs() > deadzone {
+                DeadzoneTransition::Deactivated
+            } else if old_value.abs() <= deadzone || new_value.signum() != old_value.signum() {
+                // It is possible for an analog input to completely pass through the deadzone
+                // between updates. In that case, both the old and new values would exceed the
+                // deadzone, but they would have opposite signs.
+                DeadzoneTransition::Activated
+            } else {
+                DeadzoneTransition::None
+            }
+        } else if new_value.abs() > deadzone {
+            DeadzoneTransition::Activated
+        } else {
+            DeadzoneTransition::None
+        }
     }
 }
 
@@ -115,11 +209,17 @@ impl<T> Default for AnalogInput<T> {
     fn default() -> Self {
         Self {
             inputs: Default::default(),
+
             just_activated: Default::default(),
             just_deactivated: Default::default(),
-            activation_threshold: DEFAULT_ACTIVATION_THRESHOLD,
+            deadzone: DEFAULT_DEADZONE,
+
+            just_activated_digital: Default::default(),
+            just_deactivated_digital: Default::default(),
+            deadzone_digital: DEFAULT_DEADZONE_DIGITAL,
         }
     }
 }
 
-const DEFAULT_ACTIVATION_THRESHOLD: AnalogInputValue = AnalogInputValue(0.1);
+const DEFAULT_DEADZONE: Deadzone = Deadzone(0.1);
+const DEFAULT_DEADZONE_DIGITAL: Deadzone = Deadzone(0.5);
