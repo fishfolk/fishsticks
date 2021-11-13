6ba6a673afd6ed5853360f0d85d09debb8a75206
PotatoTech <42846367+PotatoTech@users.noreply.github.com>
2021-11-13 12:00:46 -0800

    Add gilrs implementation

---
+++ src/backend/gilrs.rs
@@ -0,0 +1,70 @@
+pub use gilrs::{Axis, Button};
+
+use crate::analog::AnalogInputValue;
+use crate::{Gamepad, GamepadId};
+use std::collections::HashMap;
+
+use crate::Result;
+
+pub type ImplementationId = gilrs::GamepadId;
+
+pub struct OwnedImplementationGamepad;
+
+pub struct ImplementationContext {
+    context: gilrs::Gilrs,
+}
+
+impl ImplementationContext {
+    pub fn new() -> Result<Self> {
+        match gilrs::Gilrs::new() {
+            Ok(context) => Ok(Self { context }),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
+
+impl super::Backend for ImplementationContext {
+    fn update(&mut self, gamepads: &mut HashMap<GamepadId, Gamepad>) -> Result<()> {
+        for (_, gamepad) in gamepads.iter_mut() {
+            gamepad.update_inputs();
+        }
+
+        while let Some(gilrs::Event { id, event, .. }) = self.context.next_event() {
+            use gilrs::EventType;
+            match event {
+                EventType::Connected => {
+                    gamepads.insert(GamepadId(id), Gamepad::new());
+
+                    #[cfg(debug_assertions)]
+                    println!("Added gamepad \"{}\"", self.context.gamepad(id).name());
+                }
+                EventType::Disconnected => {
+                    gamepads.remove(&GamepadId(id));
+
+                    #[cfg(debug_assertions)]
+                    println!("Removed gamepad \"{}\"", self.context.gamepad(id).name());
+                }
+                EventType::AxisChanged(axis, value, _) => {
+                    if let Some(gamepad) = gamepads.get_mut(&GamepadId(id)) {
+                        gamepad
+                            .analog_inputs
+                            .set(axis, AnalogInputValue::from(value));
+                    }
+                }
+                EventType::ButtonPressed(button, _) => {
+                    if let Some(gamepad) = gamepads.get_mut(&GamepadId(id)) {
+                        gamepad.digital_inputs.activate(button);
+                    }
+                }
+                EventType::ButtonReleased(button, _) => {
+                    if let Some(gamepad) = gamepads.get_mut(&GamepadId(id)) {
+                        gamepad.digital_inputs.deactivate(button);
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        Ok(())
+    }
+}
